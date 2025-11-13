0cffa4fb6b2e34050d6b233cea8d3b665d1256ea
Abhisheky77 <123infinity890@gmail.com>
2025-11-13 20:36:19 +0530

    Body

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,10 +1,37 @@
 import RestaurantCard from '../Components/RestaurantCard';
 import restaurantList from "../utils/mockData";
-
-
+import { useState,useEffect} from 'react';
 
 
 function Body() {
+  const [RL, setRL] = useState(restaurantList);
+
+  const topCard = () => {
+    const ans = RL.filter((res) => res.data.avgRating > 4);
+    setRL(ans);
+  }
+
+  /* Fetch kar na ka do tarike ha 1 jo java script khud karta aur 2 ya ha loading --> Render --> API ---> Render.
+   ==> start hote hi loading hoga aur skeleton render kar de ga phir API ko request bhaje ga data aate hi phir render ho jaya ga */
+
+  const dataFetch = async () =>{
+    const data = await fetch("https://dummyjson.com/recipes");
+     const json =await data.json();
+
+    //  setRL(json)
+     console.log(json);
+     
+  }
+
+  useEffect(()=>{
+    dataFetch();
+  },[])
+
+
+// if(RL.length === 0){
+//   return <h2>Loading....</h2>
+// }
+
   return (
     <div className='body'>
 
@@ -13,9 +40,17 @@ function Body() {
         <button className='btn'>Search</button>
       </div>
 
+      <div className='filter-btn'>
+        <button className='top-filter' onClick={topCard}>Top Rated items</button>
+
+
+      </div>
+
+
+
       <div className='res-conatainer'>
         {
-          restaurantList.map(restaurant => <RestaurantCard key={restaurant.data.id} resData={restaurant} />)
+          RL.map(restaurant => <RestaurantCard key={restaurant.data.id} resData={restaurant} />)
         }
       </div>
     </div>
